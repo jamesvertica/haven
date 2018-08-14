186a1f10848ec9ef8b720fbaa4f01b8439e46cee
Maureen and John McCurley <maureenandjohn@Maureens-iMac.local>
2018-08-13 22:27:59 -0400

    index

---
+++ client/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { isSignedIn } from "./Login/UserAuth";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      signedIn: false,
+      checkedSignIn: false
+    };
+  }
+
+  componentDidMount() {
+    isSignedIn()
+      .then(res => this.setState({ signedIn: res, checkedSignIn: true }))
+      .catch(err => alert("An error occurred"));
+  }
+
+  render() {
+    const { checkedSignIn, signedIn } = this.state;
+
+    // If we haven't checked AsyncStorage yet, don't render anything (better ways to do this)
+    if (!checkedSignIn) {
+      return null;
+    }
+    //Referenc Chris's createNavigator
+    const Layout = createRootNavigator(signedIn);
+    return <Layout />;
+  }
+}
